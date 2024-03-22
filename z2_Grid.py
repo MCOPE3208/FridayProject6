5724dd51ff5f040f988d4edeb86db7c3d3d1d5ec
Makenzie Cope <mcope3208@gmail.com>
2024-03-22 09:28:34 -0500

    Added the password box and label

--- z2_Grid.py
+++ z2_Grid.py
@@ -5,7 +5,6 @@ root = Tk()
 root.title("Sign Up Page")
 root.geometry("250x150")
 
-
 name_label = ttk.Label(root, text="Name:")
 name_label.grid(row=0, column=0)
 name_entry = ttk.Entry(root)
@@ -15,5 +14,10 @@ email_label = ttk.Label(root, text="Email:")
 email_label.grid(row=1, column=0)
 email_entry = ttk.Entry(root)
 email_entry.grid(row=1, column=1)
+
+password_label = ttk.Label(root, text="Password:")
+password_label.grid(row=2, column=0)
+password_entry = ttk.Entry(root)
+password_entry.grid(row=2, column=1)
 
 root.mainloop()
