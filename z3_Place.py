0497946d78ada41914e45cac0244a88b133490be
Makenzie Cope <mcope3208@gmail.com>
2024-03-22 09:31:32 -0500

    Placed the pass word box

--- z3_Place.py
+++ z3_Place.py
@@ -10,4 +10,9 @@ username_label.place(x=50, y=50)
 username_entry = ttk.Entry(root)
 username_entry.place(x=150, y=50)
 
+password_label = ttk.Label(root, text="Password:")
+password_label.place(x=50, y=80)
+password_entry = ttk.Entry(root)
+password_entry.place(x=150, y=80)
+
 root.mainloop()
