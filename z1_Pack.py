09c1597dfde6087bbc5623c50eda91c037092e89
Makenzie Cope <mcope3208@gmail.com>
2024-03-21 20:11:26 -0500

    added buttons for 7,8,9,/ under the entry box

--- z1_Pack.py
+++ z1_Pack.py
@@ -19,5 +19,16 @@ button_frame3.pack()
 button_frame4 = Frame(root)
 button_frame4.pack()
 
+btn7 = ttk.Button(button_frame1, text = 7)
+btn7.pack(side = "left")
+
+btn8 = ttk.Button(button_frame1, text = 8)
+btn8.pack(side = "left")
+
+btn9 = ttk.Button(button_frame1, text = 9)
+btn9.pack(side = "left")
+
+btndivide = ttk.Button(button_frame1, text = "/")
+btndivide.pack(side = "left")
 
 root.mainloop()
