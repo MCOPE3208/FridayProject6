f5fd126d381a02f1dcc02082aab4fb1439401353
Makenzie Cope <mcope3208@gmail.com>
2024-03-21 20:10:00 -0500

    Added 4 frames in order to seperate the numbers

--- z1_Pack.py
+++ z1_Pack.py
@@ -7,5 +7,17 @@ root.title("Simple Calculator")
 entry_box = ttk.Entry(root, width=50, state='disabled')
 entry_box.pack(padx=10, pady=10)
 
+button_frame1 = Frame(root)
+button_frame1.pack()
+
+button_frame2 = Frame(root)
+button_frame2.pack()
+
+button_frame3 = Frame(root)
+button_frame3.pack()
+
+button_frame4 = Frame(root)
+button_frame4.pack()
+
 
 root.mainloop()
