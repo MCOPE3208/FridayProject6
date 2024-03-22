f889a5b78fd281ccc46c85679e050a84e84f66e4
Makenzie Cope <mcope3208@gmail.com>
2024-03-22 09:30:21 -0500

    Added the window label and geometry measure

---
+++ z3_Place.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("Login Page")
+root.geometry("375x300")
+
+username_label = ttk.Label(root, text="Username:")
+username_label.place(x=50, y=50)
+username_entry = ttk.Entry(root)
+username_entry.place(x=150, y=50)
+
+password_label = ttk.Label(root, text="Password:")
+password_label.place(x=50, y=80)
+password_entry = ttk.Entry(root)
+password_entry.place(x=150, y=80)
+
+login_button = ttk.Button(root, text="Login")
+login_button.place(x=175, y=120)
+
+root.mainloop()
